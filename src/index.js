153a0d9cf338da28be40de40070413a28426553c
NaginaAfzali8is <naginamafzal@i8is.com>
2024-07-26 17:58:17 +0500

    index.js

--- src/index.js
+++ src/index.js
@@ -24,39 +24,4 @@ root.render(
     </QueryClientProvider>
   </Provider>
 );
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { store } from './store/store';
-// App.js or index.js
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-// ReactDOM.render(
-//   <QueryClientProvider client={queryClient}>
-//     <MyComponent />
-//   </QueryClientProvider>,
-//   document.getElementById('root')
-// );
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 2, // 2 minutes
-      cacheTime: 1000 * 60 * 2, // 2 minutes
-      retry: 1,
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </Provider>
-);
